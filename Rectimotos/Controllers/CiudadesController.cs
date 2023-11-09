fcc15fe6b1ec0d8c1978b7bde08a6c27afe28a10
Liliana Arias <lilianarivera310@gmail.com>
2023-11-08 22:08:43 -0500

    Actualizaciones de ciudades y estados

---
+++ Rectimotos/Controllers/CiudadesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rectimotos.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rectimotos.Controllers
+{
+    public class CiudadesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CiudadesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ciudades
+        public async Task<IActionResult> Index()
+        {
+            return _context.Ciudad != null ?
+                        View(await _context.Ciudad.ToListAsync()) :
+                        Problem("Entity set 'DataContext.Ciudad'  is null.");
+        }
+
+        // GET: Ciudades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Ciudad == null)
+            {
+                return NotFound();
+            }
+
+            var ciudadViewModel = await _context.Ciudad
+                .FirstOrDefaultAsync(m => m.IdCiudad == id);
+            if (ciudadViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciudadViewModel);
+        }
+
+        // GET: Ciudades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ciudades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCiudad,Nombre")] CiudadViewModel ciudadViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ciudadViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciudadViewModel);
+        }
+
+        // GET: Ciudades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Ciudad == null)
+            {
+                return NotFound();
+            }
+
+            var ciudadViewModel = await _context.Ciudad.FindAsync(id);
+            if (ciudadViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(ciudadViewModel);
+        }
+
+        // POST: Ciudades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCiudad,Nombre")] CiudadViewModel ciudadViewModel)
+        {
+            if (id != ciudadViewModel.IdCiudad)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ciudadViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CiudadViewModelExists(ciudadViewModel.IdCiudad))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciudadViewModel);
+        }
+
+        // GET: Ciudades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Ciudad == null)
+            {
+                return NotFound();
+            }
+
+            var ciudadViewModel = await _context.Ciudad
+                .FirstOrDefaultAsync(m => m.IdCiudad == id);
+            if (ciudadViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciudadViewModel);
+        }
+
+        // POST: Ciudades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Ciudad == null)
+            {
+                return Problem("Entity set 'DataContext.Ciudad'  is null.");
+            }
+            var ciudadViewModel = await _context.Ciudad.FindAsync(id);
+            if (ciudadViewModel != null)
+            {
+                _context.Ciudad.Remove(ciudadViewModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CiudadViewModelExists(int id)
+        {
+            return (_context.Ciudad?.Any(e => e.IdCiudad == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Rectimotos/Controllers/EstadosController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rectimotos.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rectimotos.Controllers
+{
+    public class EstadosController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EstadosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Estados
+        public async Task<IActionResult> Index()
+        {
+            return _context.Estados != null ?
+                        View(await _context.Estados.ToListAsync()) :
+                        Problem("Entity set 'DataContext.Estados'  is null.");
+        }
+
+        // GET: Estados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadosViewModel = await _context.Estados
+                .FirstOrDefaultAsync(m => m.IdEstado == id);
+            if (estadosViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(estadosViewModel);
+        }
+
+        // GET: Estados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdEstado,Nombre")] EstadosViewModel estadosViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estadosViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estadosViewModel);
+        }
+
+        // GET: Estados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadosViewModel = await _context.Estados.FindAsync(id);
+            if (estadosViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(estadosViewModel);
+        }
+
+        // POST: Estados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEstado,Nombre")] EstadosViewModel estadosViewModel)
+        {
+            if (id != estadosViewModel.IdEstado)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estadosViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EstadosViewModelExists(estadosViewModel.IdEstado))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(estadosViewModel);
+        }
+
+        // GET: Estados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Estados == null)
+            {
+                return NotFound();
+            }
+
+            var estadosViewModel = await _context.Estados
+                .FirstOrDefaultAsync(m => m.IdEstado == id);
+            if (estadosViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(estadosViewModel);
+        }
+
+        // POST: Estados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Estados == null)
+            {
+                return Problem("Entity set 'DataContext.Estados'  is null.");
+            }
+            var estadosViewModel = await _context.Estados.FindAsync(id);
+            if (estadosViewModel != null)
+            {
+                _context.Estados.Remove(estadosViewModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EstadosViewModelExists(int id)
+        {
+            return (_context.Estados?.Any(e => e.IdEstado == id)).GetValueOrDefault();
+        }
+    }
+}
